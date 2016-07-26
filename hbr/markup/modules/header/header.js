e603e6055df6511f45dac0757436610c8453053c
Ruslan Polonkoev <suslik87@gmail.com>
2016-07-26 17:21:36 +0300

    hbr & start of singapore mobile

--- hbr/markup/modules/header/header.js
+++ hbr/markup/modules/header/header.js
@@ -10,4 +10,9 @@ $(function () {
         e.preventDefault();
         $('.side-menu__wrapper').removeClass('side-menu__wrapper--active');
     });
+
+    $('.js-side-menu-open-content').on('click', function (e) {
+        e.preventDefault();
+        $(this).closest('.side-menu__list-item').find('.side-menu__list-item-content').toggleClass('side-menu__list-item-content--opened');
+    });
 });
