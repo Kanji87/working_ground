06ec683b7a92b80b56411260a9884c36d61c9cc4
Ruslan Polonkoev <suslik87@gmail.com>
2016-08-29 18:16:39 +0300

    bigplus

--- bigplus_admin/markup/components/head/data/data.js
+++ bigplus_admin/markup/components/head/data/data.js
@@ -2,5 +2,32 @@ head: {
     defaults: {
         title: 'default title',
         useSocialMetaTags: false
+    },
+    users: {
+        title: 'Пользователи'
+    },
+    add_users: {
+        title: 'Добавить пользователя'
+    },
+    clinic: {
+        title: 'Клиники'
+    },
+    add_clinic: {
+        title: 'Добавить клинику'
+    },
+    library: {
+        title: 'Библиотеки'
+    },
+    faq: {
+        title: 'FAQ'
+    },
+    report: {
+        title: 'Отчеты'
+    },
+    import: {
+        title: 'Импорт'
+    },
+    mailing: {
+        title: 'Рассылки'
     }
 }
