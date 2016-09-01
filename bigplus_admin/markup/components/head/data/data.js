830a62dd63526b792b69a28b3a1b56e99bb9fe89
Ruslan Polonkoev <suslik87@gmail.com>
2016-09-01 12:59:49 +0300

    bp admin fix

--- bigplus_admin/markup/components/head/data/data.js
+++ bigplus_admin/markup/components/head/data/data.js
@@ -6,8 +6,11 @@ head: {
     users: {
         title: 'Пользователи'
     },
-    add_users: {
+    add_user: {
         title: 'Добавить пользователя'
+    },
+    edit_user: {
+        title: 'Редактировать пользователя'
     },
     clinic: {
         title: 'Клиники'
@@ -15,8 +18,17 @@ head: {
     add_clinic: {
         title: 'Добавить клинику'
     },
+    add_clinic_net: {
+        title: 'Добавить сеть клиник'
+    },
+    edit_clinic_net: {
+        title: 'Редактировать сеть клиник'
+    },
     library: {
-        title: 'Библиотеки'
+        title: 'Библиотека'
+    },
+    add_library: {
+        title: 'Добавить библиотеку'
     },
     faq: {
         title: 'FAQ'
