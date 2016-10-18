bb65395ea7c94f3cb736cff70a33607d67c56fc4
Ruslan Polonkoev <suslik87@gmail.com>
2016-10-18 17:32:41 +0300

    pre final build

--- Quick_Resto/markup/components/head/data/data.js
+++ Quick_Resto/markup/components/head/data/data.js
@@ -1,6 +1,47 @@
 head: {
     defaults: {
         title: 'default title',
-        useSocialMetaTags: true
+        desc: 'desc',
+        url: '',
+        img: '%=static=%img/general/share-pic@2x.jpg',
+        useSocialMetaTags: true,
+        fotorama: true,
+        share: false
+    },
+    article: {
+        title: 'Новые форматы — драйверы роста',
+        desc: 'Трафик заведений быстрого питания растет, несмотря на макроэкономические трудности: будущее за гибкими, мобильными и современными форматами',
+        url: '',
+        img: '%=static=%img/general/share-pic@2x.jpg',
+        useSocialMetaTags: true,
+        fotorama: false,
+        share: true
+    },
+    article1: {
+        title: 'Франчайзинг: автоматизация минимизирует риски',
+        desc: 'Несмотря на экономические трудности в стране, 34% россиян готовы открыть свой бизнес. Но сейчас организовать стабильное предприятие с минимальными рисками и высоким доходом не так просто.',
+        url: '',
+        img: '%=static=%img/general/share-pic@2x.jpg',
+        useSocialMetaTags: true,
+        fotorama: false,
+        share: true
+    },
+    article2: {
+        title: 'Как открыть собственную точку питания',
+        desc: 'Концепция общественного питания за последние несколько лет изменилась кардинально. Если раньше точки быстрого питания предлагали очень ограниченный и скудный выбор, то сейчас появилось множество новых форматов. Соответственно, меняются и схемы запуска бизнеса',
+        url: '',
+        img: '%=static=%img/general/share-pic@2x.jpg',
+        useSocialMetaTags: true,
+        fotorama: false,
+        share: true
+    },
+    article3: {
+        title: 'Особенности нового законодательства',
+        desc: 'Принятый этим летом закон кардинально изменит работу большинства предпринимателей уже с 2017 года. К чему надо готовиться и почему без интернета и современных технологий не обойтись уже никому?',
+        url: '',
+        img: '%=static=%img/general/share-pic@2x.jpg',
+        useSocialMetaTags: true,
+        fotorama: false,
+        share: true
     }
 }
--- Quick_Resto/markup/components/head/head.js
+++ Quick_Resto/markup/components/head/head.js
@@ -2,11 +2,28 @@
 
 $(function () {
     // if ( window.matchMedia('(min-width: 1280px)').matches ) {
-    //     $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">').prepend('<link href="/static/css/main.css" rel="stylesheet" type="text/css">');
-    // } else {
-    //     $('head').prepend('<meta name="viewport" content="width=320, minimal-ui">').prepend('<link href="/static/css/separate-css/mobile.css" rel="stylesheet" type="text/css">');
-    // }
+    if ( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">');
+    } else {
+        $('head').prepend('<meta name="viewport" content="width=320, minimal-ui">');
+        $('body').addClass('mobile');
+    }
 
-    $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">').prepend('<link href="/static/css/main.css" rel="stylesheet" type="text/css">');
+    // $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">').prepend('<link href="/static/css/main.css" rel="stylesheet" type="text/css">');
+
+    if ( $('.c-tile__fotorama').length ) {
+        $('.c-tile__fotorama').fotorama({
+            width: '100%',
+            height: 498,
+            navwidth: 270,
+            arrows: false,
+            shadows: false,
+            swipe: true,
+            click: true,
+            autoplay: 5000,
+            loop: true,
+            stopautoplayontouch: false
+        });
+    }
 });
 
