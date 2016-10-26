a682fbff51c2e91f09062c85a347f458d6c4d5bf
Ruslan Polonkoev <suslik87@gmail.com>
2016-10-26 15:40:23 +0300

    qr fixes

--- Quick_Resto/markup/components/head/head.js
+++ Quick_Resto/markup/components/head/head.js
@@ -4,12 +4,15 @@ $(function () {
     // if ( window.matchMedia('(min-width: 1280px)').matches ) {
     if ( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">');
+        console.log('desktop');
     } else {
         if ( window.matchMedia('(min-width: 750px').matches ) {
-            $('head').prepend('<meta name="viewport" content="width=device-width>');
+            $('head').prepend('<meta name="viewport" content="width=device-width">');
+            console.log('tablet');
         } else {
             $('head').prepend('<meta name="viewport" content="width=320, minimal-ui">');
             $('body').addClass('mobile');
+            console.log('mobile');
         }
     }
 
